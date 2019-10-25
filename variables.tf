0fafd5bcfa1cd25abce2ed1ba14554371fcc957d
Chris Gilmer <chris@truss.works>
2019-10-25 13:34:41 -0700

    Update module to include pagerduty integration and add LICENSE and terraform version.

--- variables.tf
+++ variables.tf
@@ -1,4 +1,9 @@
-variable "sns_topic_name" {
+variable "sns_topic_name_slack" {
   type        = "string"
-  description = "The name of the SNS topic to send AWS GuardDuty findings."
+  description = "The name of the Slack SNS topic to send AWS GuardDuty findings."
+}
+
+variable "sns_topic_name_pagerduty" {
+  type        = "string"
+  description = "The name of the PagerDuty SNS topic to send AWS GuardDuty findings."
 }
